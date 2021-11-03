ab640ad02f3f1d30cfff3fdc63647d0b3dbd058a
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-03 22:12:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,62 @@
 <!DOCTYPE html>
+<html>
 <head>
-
+	<title>Amazin Kalsel</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <a href="HalamanBeranda/beranda.php">Press this</a> 
+
+
+<header>
+ 	<nav>
+ 	<img src="img/logoamazingkalsel.png" alt="logo">
+        <ul>
+			<li><a href="index.php?page=home">RELIGI</a></li>
+			<li><a href="index.php?page=tentang">KULINER</a></li>
+			<li><a href="alam.php?page=alam">ALAM</a></li>
+            <li><a href="index.php?page=tutorial">HOME</a></li>
+		</ul>
+    </nav>
+    </header>
+ 
+	<div class="badan">
+ 
+ 
+	<?php 
+	if(isset($_GET['page'])){
+		$page = $_GET['page'];
+ 
+		switch ($page) {
+			case 'home':
+				include "halaman/home.php";
+				break;
+			case 'tentang':
+				include "halaman/tentang.php";
+				break;
+			case 'tutorial':
+				include "halaman/tutorial.php";
+				break;			
+			default:
+				echo "<center><h3>Maaf. Halaman tidak di temukan !</h3></center>";
+				break;
+		}
+	}else{
+		include "halaman/home.php";
+	}
+ 
+	 ?>
+ 
+	</div>
+
+<div class="footer">
+        
+		<ul>
+			<li><a>Copyright 2021 - Amazing Kalsel</a></li>
+
+		</ul>
+	</div>
+
+</div>
 </body>
+</html>
