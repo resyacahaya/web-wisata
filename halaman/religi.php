b57f918bda9f307d3a1c3eade624929b9afbcfd9
HfzYan <90849858+HfzYan@users.noreply.github.com>
2021-11-04 08:14:55 +0800

    Create religi.php

---
+++ halaman/religi.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Amazing Kalsel - Alam</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+<body>
+
+ 	<nav>
+ 	<img src="/img/logoamazingkalsel.png" alt="logo">
+        <ul>
+			<li><a href="alam.php?page=alam">ALAM</a></li>
+			<li><a href="kuliner.php?page=kuliner">KULINER</a></li>
+      <li><a href="religi.php?page=religi">RELIGI</a></li>
+		</ul>
+    </nav>
+  <?php echo "Halaman Religi" ?>
+  <div class="footer">
+        
+		<ul>
+			<li><a>Copyright 2021 - Amazing Kalsel</a></li>
+
+		</ul>
+	</div>
+
+</div>
+</body>
+</html>
