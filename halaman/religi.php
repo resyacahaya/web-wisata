d89f98ccf958ec0ffc44015f83ea54ceaceeca08
aryopratama100 <90923023+aryopratama100@users.noreply.github.com>
2021-11-09 22:07:45 +0800

    update religi.php

--- halaman/religi.php
+++ halaman/religi.php
@@ -2,28 +2,97 @@
 <html>
 <head>
 	<title>Amazing Kalsel - Alam</title>
-	<!-- menghubungkan dengan file css -->
-	<link rel="stylesheet" type="text/css" href="/style.css">
+<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="../css/objek-wisata.css">
 </head>
 <body>
-
+<!-- Navbar -->
  	<nav>
- 	<img src="/img/logoamazingkalsel.png" alt="logo">
-        <ul>
-			<li><a href="alam.php?page=alam">ALAM</a></li>
-			<li><a href="kuliner.php?page=kuliner">KULINER</a></li>
-      <li><a href="religi.php?page=religi">RELIGI</a></li>
-		</ul>
-    </nav>
-  <?php echo "Halaman Religi" ?>
-  <div class="footer">
+ 		<a href="../index.php">
+			<img src="../img/logoamazingkalsel.png" alt="logo">
+				<ul>
+					<li><a href="alam.php?page=alam">ALAM</a></li>
+					<li><a href="kuliner.php?page=kuliner">KULINER</a></li>
+					<li><a href="religi.php?page=religi">RELIGI</a></li>
+				</ul>
+			</nav>
+<!-- Banner -->
+    <div class="container">
+      <div class="header">
+          <img src="../img/banner-religi.png" width="100%" height="500">
+      </div>
+
+<!-- Konten -->
+
+        <section id="konten">
+                <div class="konten-list">
+                    <div class="objek-wisata1">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata2">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata3">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+		<section id="konten">
+                <div class="konten-list">
+                    <div class="objek-wisata1">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata2">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata3">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+		<section id="konten">
+                <div class="konten-list">
+                    <div class="objek-wisata1">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata2">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata3">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+		<section id="konten">
+                <div class="konten-list">
+                    <div class="objek-wisata1">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata2">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                    <div class="objek-wisata3">
+                        <img src="../img/konten-religi.png"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+		
+<!-- Footer -->
+<div class="footer">
         
 		<ul>
-			<li><a>Copyright 2021 - Amazing Kalsel</a></li>
-
+			<li><a>Copyright &#169; 2021 - Amazing Kalsel</a></li>
+            <img src="../img/icon-ig.png" alt="instagram">
 		</ul>
+        
 	</div>
-
 </div>
 </body>
 </html>
