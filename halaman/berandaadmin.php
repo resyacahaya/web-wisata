e2abab4c918233147e902fdaeb7b348a8573ef5b
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2021-11-09 23:30:51 +0800

    memperbaiki link gambar berandaadmin.php

--- halaman/berandaadmin.php
+++ halaman/berandaadmin.php
@@ -10,12 +10,12 @@
 
 <div class="wrapper">
     <div class="sidebar">
-        <h2><img src="img/logoamazingkalselputih.png"></a></h2>
+        <h2><img src="../img/logoamazingkalselputih.png"></a></h2>
         <ul>
             <li><a href="#"><img src="../img/admin/icon-home.png">Home</a></li>
-            <li><a href="#"><img src="img/admin/icon-alam.png">Alam</a></li>
-            <li><a href="#"><img src="img/admin/icon-kuliner.png">Kuliner</a></li>
-            <li><a href="#"><div class="religi"><img src="img/admin/icon-religi.png">Religi</div></a></li>
+            <li><a href="#"><img src="../img/admin/icon-alam.png">Alam</a></li>
+            <li><a href="#"><img src="../img/admin/icon-kuliner.png">Kuliner</a></li>
+            <li><a href="#"><div class="religi"><img src="../img/admin/icon-religi.png">Religi</div></a></li>
         </ul>
     </div>
 
