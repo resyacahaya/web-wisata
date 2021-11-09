512359f5b70738ccd8420bcbc9b05cc3382a1092
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2021-11-09 23:05:57 +0800

    berandaadmin.php menambahkan kerangka sidebar

---
+++ halaman/berandaadmin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Beranda Admin</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar">
+        <h2><img src="img/logoamazingkalselputih.png"></a></h2>
+        <ul>
+            <li><a href="#"><img src="img/admin/icon-home.png">Home</a></li>
+            <li><a href="#"><img src="img/admin/icon-alam.png">Alam</a></li>
+            <li><a href="#"><img src="img/admin/icon-kuliner.png">Kuliner</a></li>
+            <li><a href="#"><div class="religi"><img src="img/admin/icon-religi.png">Religi</div></a></li>
+        </ul>
+    </div>
+</div>
+
+</body>
+</html>
