e402318672ce846f61d2996c75b397b25a78711a
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2021-11-10 01:37:16 +0800

    menambahkan codingan berandaadmin.php

--- halaman/berandaadmin.php
+++ halaman/berandaadmin.php
@@ -11,17 +11,18 @@
 <div class="wrapper">
     <div class="sidebar">
         <h2><img src="../img/logoamazingkalselputih.png"></a></h2>
-        <ul>
+        <div class="menu">
+          <ul>
             <li><a href="#"><img src="../img/admin/icon-home.png">Home</a></li>
             <li><a href="#"><img src="../img/admin/icon-alam.png">Alam</a></li>
             <li><a href="#"><img src="../img/admin/icon-kuliner.png">Kuliner</a></li>
             <li><a href="#"><div class="religi"><img src="../img/admin/icon-religi.png">Religi</div></a></li>
-        </ul>
+          </ul> 
+        </div>
     </div>
-
     <div class="main_content">
         <div class="header1">
-          <b>hmmmmm</b>
+          <b>Admin</b><button type="button">Logout</button>
         </div>  
         <div class="header2">
           <b>Hello, Admin</b>
@@ -29,25 +30,30 @@
 
         <div class="container">
           <div class="col-1">
+            <div class="kotak"><img src="../img/admin/icon-home.png"></div>
             <br><h3>Beranda Traveler</h3>
             <br><p>Beranda Traveler adalah tab utama ketika traveler pertama kali mengakses website.</p>
           </div>
-          <div class="col-1">
+          <div class="col-2">
+            <div class="kotak"><img src="../img/admin/icon-alam.png"></div>
             <br><h3>Alam</h3>
             <br><p>Rekomendasi destinasi wisata yang berhubungan dengan alam, seperti pantai, sungai, air terjun, gunung, dan masih banyak lagi.</p>
           </div>
         </div>
 
         <div class="container">
-          <div class="col-1">
+          <div class="col-3">
+            <div class="kotak"><img src="../img/admin/icon-kuliner.png"></div>
             <br><h3>Kuliner</h3>
             <br><p>Rekomendasi tempat wisata kuliner tradisional khas Kalimantan Selatan.</p>
           </div>
-          <div class="col-1">
+          <div class="col-4">
+            <div class="kotak1"><img src="../img/admin/icon-religi.png"></div>
             <br><h3>Religi</h3>
             <br><p>Rekomendasi destinasi wisata yang berhubungan dengan tempat ibadah dan kegiatan atau event keagamaan.</p>
           </div>
         </div>
+
     </div>
 </div>
 
