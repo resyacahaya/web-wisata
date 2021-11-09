e989c2f5c7db2243a9e3ccc973856184bf83f7c8
rahmadaniroslan <1910817720001@mhs.ulm.ac.id>
2021-11-09 12:02:39 +0800

    menambah file login dan css

---
+++ halaman/loginadmin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Amazing Kalsel</title>
+	<!-- menghubungkan dengan file css -->
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+ 	<img src="img/logoamazingkalsel.png" alt="logo">
+</div>
+</div>
+        <div class="container">
+          <h1>LOGIN ADMIN</h1>
+            <form>
+                <label>Username</label><br>
+                <input type="text"><br>
+                <label>Password</label><br>
+                <input type="password"><br>
+                <button>Masuk</button>
+            </form>
+        </div>     
+    </body>
+</html>
