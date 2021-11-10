4921ad5a38dd571f32cabd79ecac12fc359ab425
rahmadani <90970109+rahmadaniroslan@users.noreply.github.com>
2021-11-11 00:58:47 +0800

    Create infoalam.php

---
+++ halaman/infoalam.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title>Amazing Kalsel - Alam</title>
+    <!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="../css/infowisata.css">
+</head>
+<body>
+<header>
+<img src="../img/logoamazingkalsel.png" alt="logo">
+				<ul>
+                    <li><a href="home.php?page=alam">Home</a></li>
+					<li><a href="alam.php?page=alam">Alam</a></li>
+					<li><a href="kuliner.php?page=kuliner">Kuliner</a></li>
+					<li><a href="religi.php?page=religi">Religi</a></li>
+				</ul>
+</header>
+<nav>
+<div class=card>
+<div class="container"> 
+	<h1>Air Terjun Mandin Mangapan</h1>
+	<img src="../img/ArterMandinMangapan.jpg" alt="gambar">
+	<div style="display: flex;">
+   <div class="container-left">
+</nav>
+
+<div class="footer">
+        
+		<ul>
+			<li><a>Copyright &#169; 2021 - Amazing Kalsel</a></li>
+            <img src="../img/icon-ig.png" alt="instagram">
+		</ul>
+        
+	</div>
+</body>
+</html>
