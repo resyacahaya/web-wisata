366a5065fda1fa88fd8a65e5270a9946e81d6fb0
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2021-11-15 22:32:24 +0800

    mengedit file alamadmin.php

--- halaman/alamadmin.php
+++ halaman/alamadmin.php
@@ -13,15 +13,20 @@
         <h2><img src="../img/logoamazingkalselputih.png"></a></h2>
         <div class="menu">
           <ul>
-            <li><a href="#"><img src="../img/admin/icon-home.png">Home</a></li>
-            <li><a href="#"><img src="../img/admin/icon-alam.png">Alam</a></li>
+            <li><a href="berandaadmin.php"><img src="../img/admin/icon-home.png">Home</a></li>
+            <li><a href="alamadmin.php"><img src="../img/admin/icon-alam.png">Alam</a></li>
             <li><a href="#"><img src="../img/admin/icon-kuliner.png">Kuliner</a></li>
             <li><a href="#"><div class="religi"><img src="../img/admin/icon-religi.png">Religi</div></a></li>
           </ul> 
         </div>
     </div>
     <div class="main_content">
-
+        <div class="header1">
+          <b>Admin</b><button type="button">Logout</button>
+        </div>  
+        <div class="header2">
+          <b>Hello, Admin</b>
+        </div>
     </div>
 </div>
 
