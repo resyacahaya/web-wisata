a6b94f7429987ef9857be7bc6d1799decfd54d52
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2021-11-15 22:36:32 +0800

    mengedit file berandaadmin.php

--- halaman/berandaadmin.php
+++ halaman/berandaadmin.php
@@ -15,8 +15,8 @@
           <ul>
             <li><a href="berandaadmin.php"><img src="../img/admin/icon-home.png">Home</a></li>
             <li><a href="alamadmin.php"><img src="../img/admin/icon-alam.png">Alam</a></li>
-            <li><a href="#"><img src="../img/admin/icon-kuliner.png">Kuliner</a></li>
-            <li><a href="#"><div class="religi"><img src="../img/admin/icon-religi.png">Religi</div></a></li>
+            <li><a href="kulineradmin.php"><img src="../img/admin/icon-kuliner.png">Kuliner</a></li>
+            <li><a href="religiadmin.php"><div class="religi"><img src="../img/admin/icon-religi.png">Religi</div></a></li>
           </ul> 
         </div>
     </div>
