804d3ab5ec2af4a1477a664f566ccd8f28615a00
rahmadani <90970109+rahmadaniroslan@users.noreply.github.com>
2021-11-16 16:45:48 +0800

    menambahinfoalam2

---
+++ halaman/infoalam2.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+	<title>Amazing Kalsel - Alam</title>
+    <!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="../css/infowisata.css">
+</head>
+<body>
+<!-- Navbar -->
+	<header>
+		<img src="../img/logoamazingkalsel.png" alt="logo">
+				<ul>
+                    <li><a href="home.php?page=alam">Home</a></li>
+					<li><a href="alam.php?page=alam">Alam</a></li>
+					<li><a href="kuliner.php?page=kuliner">Kuliner</a></li>
+					<li><a href="religi.php?page=religi">Religi</a></li>
+				</ul>
+	</header>
+	<nav>
+		<div class=card>
+		<div class="container" align="left"> 
+		<h1>Bendungan Tapin</h1>
+		<img src="../img/wisata-alam/BendunganTapin.jpg" alt="gambar">
+		<div style="display: flex;">
+   		<div class="container-left">
+	   <table border="0,8">
+   		<td><font>Bendungan Tapin atau bisa juga disebut Bendungan Pipitak Jaya terletak di wilayah<br><br>
+                Desa Pipitak Jaya Kecamatan Piani Kabupaten Tapin. Bendungan Tapin memiliki view <br><br>
+                yang indah karena berada Pegunungan Meratus. Bendungan Tapin dapat menyediakan<br><br> 
+                air baku untuk wilayah Kota Rantau, Kalimantan Selatan, sebesar 500 liter per detik.<br><br> 
+                Bendungan ini disebut mampu mereduksi banjir sebesar 107 meter kubik per detik. <br><br> 
+                Bendungan Tapin juga dapat dijadikan konservasi air (ground water recharge), <br><br>
+                dan destinasi wisata di kawasan Daerah Aliran Sungai (DAS) Tapin.
+        </td></font>
+			</table>
+	</nav>
+		<div class="footer">
+			<ul>
+			<li><a>Copyright &#169; 2021 - Amazing Kalsel</a></li>
+            <img src="../img/icon-ig.png" alt="instagram">
+			</ul>
+        	</div>
+</body>
+</html>
