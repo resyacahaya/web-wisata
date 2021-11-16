9125db689642c4d070a4760d89d4007acf27b725
rahmadani <90970109+rahmadaniroslan@users.noreply.github.com>
2021-11-16 16:02:53 +0800

    update info

--- halaman/infoalam.php
+++ halaman/infoalam.php
@@ -9,38 +9,34 @@
 	<header>
 		<img src="../img/logoamazingkalsel.png" alt="logo">
 				<ul>
-                    			<li><a href="home.php?page=alam">Home</a></li>
+                    <li><a href="home.php?page=alam">Home</a></li>
 					<li><a href="alam.php?page=alam">Alam</a></li>
 					<li><a href="kuliner.php?page=kuliner">Kuliner</a></li>
 					<li><a href="religi.php?page=religi">Religi</a></li>
 				</ul>
 	</header>
-<nav>
-<div class=card>
-<div class="container" align="left"> 
-	<h1>Air Terjun Mandin Mangapan</h1>
-	<img src="../img/ArterMandinMangapan.jpg" alt="gambar">
-	<div style="display: flex;">
-   <div class="container-left">
+	<nav>
+		<div class=card>
+		<div class="container" align="left"> 
+		<h1>Air Terjun Mandin Mangapan</h1>
+		<img src="../img/ArterMandinMangapan.jpg" alt="gambar">
+		<div style="display: flex;">
+   		<div class="container-left">
 	   <table border="0,8">
-   <td><font face="sans-serif" size="4" color="#777777" align="right">Air Terjun Mandin mangapan berlokasi di Desa Paramasan Atas, Kecamatan Peramasan, <br>
-Kabupaten Banjar, Kalimantan Selatan, sebuah air terjun dengan ketinggian lebih <br>
-kurang 30 meter, berada di pedalaman kalimantan selatan pasnya di Gunung Ambil atau <br> 
-Desa Paramasan Atas, Jarak untuk menempuh perjalanan kurang lebih akan menghabiskan <br>
-waktu selama 3 jam menuju Kecamatan Paramasan didalam perjalanan pengunjung akan <br>
-melewati lokasi pedesaan Bancing dan Peramasan atas yang dihuni oleh masyarakat <br>
-		suku Dayak Paramasan.</td>
-</table>
-</nav>
-	
-
-<div class="footer">
-        
-		<ul>
+   		<td><font>Air Terjun Mandin mangapan berlokasi di Desa Paramasan Atas, Kecamatan Peramasan, <br><br>
+				Kabupaten Banjar, Kalimantan Selatan, sebuah air terjun dengan ketinggian lebih <br><br>
+				kurang 30 meter, berada di pedalaman kalimantan selatan pasnya di Gunung Ambil atau <br> <br>
+				Desa Paramasan Atas, Jarak untuk menempuh perjalanan kurang lebih akan menghabiskan <br> <br>
+				waktu selama 3 jam menuju Kecamatan Paramasan didalam perjalanan pengunjung akan <br><br>
+				melewati lokasi pedesaan Bancing dan Peramasan atas yang dihuni oleh masyarakat <br><br>
+				Suku Dayak Paramasan.</td></font>
+			</table>
+	</nav>
+		<div class="footer">
+			<ul>
 			<li><a>Copyright &#169; 2021 - Amazing Kalsel</a></li>
             <img src="../img/icon-ig.png" alt="instagram">
-		</ul>
-        
-	</div>
+			</ul>
+        	</div>
 </body>
 </html>
