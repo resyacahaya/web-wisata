a95c25d0c8d0b6dbaa8ea647af7540cbe39c460c
IlhamRasyid111 <m.ilhamridha27@gmail.com>
2021-11-17 14:52:53 +0800

    menambahkan file editalam.php editkuliner.php editreligi.php

---
+++ halaman/editkuliner.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Edit</title>
+	<link rel="stylesheet" type="text/css" href="../css/edit.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+<div class="wrapper">
+	<div class="main_content">
+		<div class="header1">
+          <b class="judul">Edit Info Wisata Kuliner</b> 
+          <b class="admin">Admin</b><button type="button">Logout</button>
+        </div>
+	</div>
+</div>
+</body>
+</html>
