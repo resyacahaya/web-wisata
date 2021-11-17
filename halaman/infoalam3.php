e1933dce9d217f3937f1cb26576e7fac431ef560
rahmadani <90970109+rahmadaniroslan@users.noreply.github.com>
2021-11-17 08:23:22 +0800

    updateinfoalam3

---
+++ halaman/infoalam3.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+	<title>Amazing Kalsel - Alam</title>
+    <!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="../css/infowisata.css">
+</head>
+<body>
+<!-- Navbar -->
+	<header>
+		<img src="../img/logoamazingkalsel.png" alt="logo">
+				<ul>
+                    <li><a href="home.php?page=alam">Home</a></li>
+					<li><a href="alam.php?page=alam">Alam</a></li>
+					<li><a href="kuliner.php?page=kuliner">Kuliner</a></li>
+					<li><a href="religi.php?page=religi">Religi</a></li>
+				</ul>
+	</header>
+	<nav>
+		<div class=card>
+		<div class="container" align="left"> 
+		<h1>Bukit Batu</h1>
+		<img src="../img/wisata-alam/BukitBatu.jpg" alt="gambar">
+		<div style="display: flex;">
+   		<div class="container-left">
+	   <table border="0,8">
+   		<td><font>Bukit batu salah satu potensi destinasi wisata Kalsel baru berupa alam perbukitan<br><br>
+               kawasan waduk Riam Kanan Kecamatan Aranio Kabupaten Banjar. Persisnya berada<br><br>
+               di desa Sungai Luar Kecamatan Aranio Kabupaten Banjar. Untuk menuju ke lokasi<br><br>
+               ini bisa melalui akses jalan darat atau lewat sungai waduk Riam Kanan. Ada<br><br> 
+               yang bisa melewati jalur darat melalui jalan Ir PHM Noor hingga ke desa<br><br> 
+               Sungai luar dan ada yang melalui jalur transportasi sungai melewati waduk<br><br> 
+               dari dermaga waduk Riam Kanan. Bukit batu ini menjadi trending belakangan.<br><br>
+               Hal itu karena menawarkan view atau pemandangan yang super indah dengan<br><br> 
+               panorama savana dan bisa melihat hamparan air di waduk dan gugusan pulau<br><br>
+               pulau kecil.
+        </td></font>
+			</table>
+	</nav>
+		<div class="footer">
+			<ul>
+			<li><a>Copyright &#169; 2021 - Amazing Kalsel</a></li>
+            <img src="../img/icon-ig.png" alt="instagram">
+			</ul>
+        	</div>
+</body>
+</html>
